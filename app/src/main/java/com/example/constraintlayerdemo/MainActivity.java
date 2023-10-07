dbaf80de4b577b6c6c5be2cf2a10321f1d3e029b
srikar <voorasrikar7@gmail.com>
2023-10-06 22:30:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/constraintlayerdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.constraintlayerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn =findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this,"Submitted",Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } );
+    }
+
+}
